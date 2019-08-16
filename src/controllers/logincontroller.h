15f8bcfcab7cb2e9336f2411b690b605f7c15cdf
litovko <lss73@mail.ru>
2019-08-16 19:16:59 +0300

    loginController added.

---
+++ src/controllers/logincontroller.h
@@ -0,0 +1,11 @@
+#ifndef LOGINCONTROLLER_H
+#define LOGINCONTROLLER_H
+
+
+class logincontroller : public public HttpRequestHandler
+{
+public:
+    logincontroller();
+};
+
+#endif // LOGINCONTROLLER_H
