4518f3a627377ef3336f58a2708ad5763a934800
litovko <sergei.litovko@gmail.com>
2019-08-16 19:38:30 +0300

    n

--- src/controllers/logincontroller.cpp
+++ src/controllers/logincontroller.cpp
@@ -12,5 +12,5 @@ void loginController::service(HttpRequest &request, HttpResponse &response)
     QByteArray path=request.getPath();
     qDebug("loginConroller: path=%s",path.data());
     response.write("{token=123}",true);
-    //curl -d '{"login":"user","password":"user"}' -H "Content-type: application/json; charset=utf-8" -H "Accept: application/json"  https://localhost/api/v1/login?appid=FFF01
+    //curl -d '{"login":"user","password":"user"}'  -H "Content-type: application/json; charset=utf-8" -H "Accept: application/json"  https://localhost/api/v1/login?appid=FFF01
 }
