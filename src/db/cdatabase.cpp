e74248b48ad80a378ca85960ba7c135a4edaf3aa
litovko <sergei.litovko@gmail.com>
2019-08-25 21:20:29 +0300

    Added GET with parameters and PUT.

--- src/db/cdatabase.cpp
+++ src/db/cdatabase.cpp
@@ -6,7 +6,9 @@
 #include <QJsonObject>
 #include <QThread>\
 
-#define DB_SERVER_HOST "192.168.224.57"
+//#define DB_SERVER_HOST "192.168.224.57"
+#define DB_SERVER_HOST "localhost"
+#define DB_DBNAME "dsydb"
 #define DB_USERNAME "postgres"
 #define DB_USERPASS "12345678"
 
@@ -18,11 +20,12 @@ QByteArray cDatabase::error(QString source, int code)
 
 cDatabase::cDatabase(QObject *parent) : QObject(parent)
 {
-    //    QSqlDatabase db = get_db();
+//        QSqlDatabase db = get_db();
 
-    //        if (db.isOpen())
-    //        {
-//            QSqlQuery query("select * from users;",db);
+//            if (db.isOpen())
+//            {
+//            QSqlQuery query("select * from users",db);
+//            qDebug() << query.lastError().text();
 //            while (query.next())
 //            {
 //                qWarning() << query.value(0).toString();
@@ -37,7 +40,9 @@ cDatabase::cDatabase(QObject *parent) : QObject(parent)
 //            qDebug() << "cannot open DB";
 //            qDebug() << db.lastError();
 //        }
-
+/*
+curl -d {\"pkey\":\"G&jK\",\"file\":\"/path/filename\",\"type\":1}  -H "Content-type: application/json; charset=utf-8" -H "Accept: application/json"  https://localhost/api/v1/tasks -k
+*/
 }
 
 QByteArray cDatabase::login(stefanfrings::HttpRequest &request)
@@ -76,6 +81,7 @@ QByteArray cDatabase::login(stefanfrings::HttpRequest &request)
 QByteArray cDatabase::tasks(stefanfrings::HttpRequest &request, QString &path)
 {
     QString sql;
+    QString param="";
     auto db=get_db();
     qDebug()<<Q_FUNC_INFO;
     QJsonDocument doc = QJsonDocument::fromJson(request.getBody());
@@ -83,20 +89,43 @@ QByteArray cDatabase::tasks(stefanfrings::HttpRequest &request, QString &path)
         qWarning()<<"bad parsing:"<<Q_FUNC_INFO;;
         return error("tasks", 1000);
     }
-    if (path.contains("/list"))  //список задач, пока смотрим только для конкретного пользователя
-    sql="select json_agg(s) tasks "
-        "from (select t.id, t.uid, t.state_id, t.type, t.param from tasks t, users u where t.uid=u.id and u.pkey='"
-            +doc["pkey"].toString()
-            +"')s";
+    if (request.getMethod()=="GET") {  //список задач, пока смотрим только для конкретного пользователя
+//curl -d {\"pkey\":\"G&jK\",\"state_id\":8}  -H "Content-type: application/json; charset=utf-8" -H "Accept: application/json"  -X GET https://localhost/api/v1/tasks?id=90 -k
+        for(auto el: request.getParameterMap().toStdMap())
+            param=param+" and t."+el.first+" = "+el.second+" ";
+        qDebug()<<"GET:"<<"param:"<<param;
+        sql="select json_agg(s) as tasks "
+            "from (select t.id, t.uid, t.state_id, t.type, t.param, t.created_at, t.updated_at, t.completed_at from tasks t, users u where t.uid=u.id and u.pkey='"
+                + doc["pkey"].toString() +"' "
+                + param
+                + ")s";
+        qDebug()<<"GET:"<<sql;
+    }
+    else if (request.getMethod()=="POST"){
+        sql="select add_task("
+            "100,'" //тип задачи, указываемой по умолчанию - 100, если тип содержиться в json, то он и присваивается
+                +request.getBody()
+                +"')";
+        qDebug()<<"POST:"<<sql;
+    }
+    else  if (request.getMethod()=="PUT") {
+        //select update_task('{"id":87,"pkey":"G&jK", "state_id":7}');
+        //curl -d {\"id\":90,\"pkey\":\"G&jK\",\"state_id\":8}  -H "Content-type: application/json; charset=utf-8" -H "Accept: application/json"  -X PUT https://localhost/api/v1/tasks -k
+        sql="select update_task('"
+            + request.getBody()
+            +"')";
+        qDebug()<<"PUT:"<<sql;
+    }
     else
-        sql="select add_task(100,'"+request.getBody()+"')"; //тип задачи по умолчанию 100
-    qDebug()<<"sql:"<<sql;
+        return error("tasks", 1004); //нобрабатываемый метод HTTP запроса.
+
+   //Debug()<<"sql:"<<sql;
     QSqlQuery query(sql,db);
     QJsonDocument ret;
     while (query.next()) {
         qWarning() << query.value(0) <<"ret string:"<<query.lastError();
         if (query.value(0).toString().length()==0)
-                    return error("tasks", 1003);
+            return error("tasks", 1003); // 0 строк в ответе
         ret = QJsonDocument::fromJson(query.value(0).toString().toUtf8());
         qDebug()<<"jason answer:"<<ret.toJson(QJsonDocument::Indented);
     }
@@ -113,9 +142,8 @@ QSqlDatabase cDatabase::get_db()
     else {
         QSqlDatabase db = QSqlDatabase::addDatabase( "QPSQL", name);
         qDebug()<<"adding DB"<<QSqlDatabase::connectionNames();
-        // open the database, setup tables, etc.
         db.setHostName(DB_SERVER_HOST);
-        db.setDatabaseName("dsydb");
+        db.setDatabaseName(DB_DBNAME);
         db.setUserName(DB_USERNAME);
         db.setPassword(DB_USERPASS);
         db.open();
